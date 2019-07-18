968adbd7370ed4fb947738aee6a8b6e4b65dcfdc
Bret R. Zaun <bretrzaun@hotmail.com>
2019-07-18 15:31:46 +0200

    composer: update dependencies phpunit: remove old configuration Test-Kernel: remove getRootDir()

--- tests/AppKernel.php
+++ tests/AppKernel.php
@@ -3,6 +3,8 @@ namespace BretRZaun\MaintenanceBundle\Tests;
 // tests/AppKernel.php (you can define it in a subdirectory /Fixtures if you prefer)
 
 use BretRZaun\MaintenanceBundle\MaintenanceBundle;
+use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
+use Symfony\Bundle\TwigBundle\TwigBundle;
 use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\Config\Loader\LoaderInterface;
 
@@ -10,10 +12,10 @@ class AppKernel extends Kernel
 {
     public function registerBundles()
     {
-        $bundles = array();
+        $bundles = [];
         if ('test' === $this->getEnvironment()) {
-            $bundles[] = new \Symfony\Bundle\FrameworkBundle\FrameworkBundle();
-            $bundles[] = new \Symfony\Bundle\TwigBundle\TwigBundle();
+            $bundles[] = new FrameworkBundle();
+            $bundles[] = new TwigBundle();
             $bundles[] = new MaintenanceBundle();
         }
         return $bundles;
@@ -22,10 +24,5 @@ class AppKernel extends Kernel
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
         $loader->load(__DIR__.'/config.yml');
-    }
-
-    public function getRootDir()
-    {
-        return dirname(parent::getRootDir()) . '/';
     }
 }
